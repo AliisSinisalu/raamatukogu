43b70b10a9c808891c4f91e6b25ae418d957c5f7
Aliis Sinisalu <aliissinisalu@yahoo.com>
2013-04-27 11:46:04 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raamatukogu::Application.config.secret_token = '5c289e3203f1a89c692a494d953dabfcb148ae617b84fe462376dee2417e5688ecf198d71a933fb52c5a3d7135b979addd003905c0f214071b8c5a747cdd1745'
